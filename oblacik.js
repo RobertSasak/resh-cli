6fd03623bad183246d58be7039aaf873b64e3c69
RobertSasak <robert.sasak@gmail.com>
2014-08-01 15:53:50 +0200

    Download files.

--- oblacik.js
+++ oblacik.js
@@ -2,6 +2,39 @@
 
 var fs = require('fs');
 var FormData = require('form-data');
+var _ = require('lodash');
+var http = require('http');
+var fs = require('fs');
+var urljoin = require('url-join');
+
+function download(url, dest, cb) {
+	//TODO: check dest is subfolder
+	var file = fs.createWriteStream(dest);
+
+	http.get(url, function (response) {
+		response.pipe(file);
+		file.on('finish', function () {
+			file.close(cb);
+		});
+	});
+}
+
+function downloadFiles(fileUrl) {
+	http.get(fileUrl, function (res) {
+		var body = '';
+
+		res.on('data', function (chunk) {
+			body += chunk;
+		}).on('end', function () {
+			var files = JSON.parse(body);
+			_.forEach(files, function (f) {
+				download(urljoin(fileUrl, f.path), f.path);
+			});
+		});
+	}).on('error', function (error) {
+		throw error;
+	});
+}
 
 function shell(userKey, server, command, files) {
 	var form = new FormData();
@@ -11,12 +44,20 @@ function shell(userKey, server, command, files) {
 		form.append(f, fs.createReadStream(file.path));
 	}
 
-	form.submit(server + '/' + encodeURIComponent(userKey) + '/' + encodeURIComponent(command), function (err, res) {
+	var url = urljoin(server, encodeURIComponent(userKey), encodeURIComponent(command));
+
+	form.submit(url, function (err, res) {
 		if (err) {
 			throw err;
-		} else {
-			res.pipe(process.stdout);
 		}
+
+		var filesUrl = res.headers['x-files-url'];
+
+		if (filesUrl) {
+			downloadFiles(server + '/' + filesUrl);
+		}
+
+		res.pipe(process.stdout);
 	});
 }
 
